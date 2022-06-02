0ffa070a4233b0b96b22db6331ee794e9dd651e5
marquisalexander <marquissantos123@gmail.com>
2022-06-02 11:31:00 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import * as SplashScreen from 'expo-splash-screen';
+import { ThemeProvider } from "styled-components";
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import {
+	Archivo_400Regular,
+	Archivo_500Medium,
+	Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+
+import { Home } from "./src/screens/Home";
+import theme from "./src/styles/theme";
+
+export default function App() {
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_500Medium,
+		Archivo_400Regular,
+		Archivo_500Medium,
+		Archivo_600SemiBold,
+	});
+
+	if (fontsLoaded) {
+		SplashScreen.preventAutoHideAsync()
+	}
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Home />
+		</ThemeProvider>
+	);
+}
