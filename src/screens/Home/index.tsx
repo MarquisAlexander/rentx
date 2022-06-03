b137871a515211e41410abc18ebb34bf23039e0d
marquisalexander <marquissantos123@gmail.com>
2022-06-03 13:48:58 -0300

    add car card

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,25 +1,49 @@
 import React from "react";
-import {StatusBar} from "react-native"
-import {RFValue} from "react-native-responsive-fontsize"
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
 
-import {Container, Header, TotalCars, HeaderContent} from "./styles";
+import { Car } from "../../components/Car";
+
+import { Container, Header, TotalCars, HeaderContent } from "./styles";
 
 export function Home() {
-    return (
-        <Container>
-            <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-            <Header>
-                <HeaderContent>
+    const carData ={
+        brand: "Audi",
+        name: "RS 5 Coupé",
+        rent: {
+            period: "AO DIA",
+            price: 120,
+        },
+        thumbnail: "https://www.pngmart.com/files/1/Audi-A7-S-Line-PNG.png",
+    }
 
-                <Logo height={RFValue(12)} width={RFValue(108)} />
-                <TotalCars>
-                    Total de 12 carros
-                </TotalCars>
-                </HeaderContent>
+    const carDataTwo ={
+        brand: "Audi",
+        name: "RS 5 Coupé",
+        rent: {
+            period: "AO DIA",
+            price: 320,
+        },
+        thumbnail: "https://www.pngmart.com/files/1/Audi-A7-S-Line-PNG.png",
+    }
 
-            </Header>
-        </Container>
-    )
+	return (
+		<Container>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
+			<Header>
+				<HeaderContent>
+					<Logo height={RFValue(12)} width={RFValue(108)} />
+					<TotalCars>Total de 12 carros</TotalCars>
+				</HeaderContent>
+			</Header>
+			<Car data={carData}/>
+			<Car data={carDataTwo}/>
+		</Container>
+	);
 }
