c52d60ec3abaecf7755960fc9f7e8ac41aa0d4f2
marquisalexander <marquis.alexander@outlook.com>
2022-07-05 19:41:51 -0300

    (feat):add new screen scheduling

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ import {
 
 import { Home } from "./src/screens/Home";
 import theme from "./src/styles/theme";
-import { CarDetails } from "./src/screens/CarDetails";
+import { Scheduling } from "./src/screens/Scheduling";
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -34,7 +34,7 @@ export default function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			{/* <Home /> */}
-			<CarDetails />
+			<Scheduling />
 		</ThemeProvider>
 	);
 }
