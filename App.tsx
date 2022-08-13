30491bb05bc134e4e0f5c5347082c1152880aacd
MarquisAlexander <marquis.alexander@outlook.com>
2022-08-12 21:32:06 -0300

    refactor():create scheduling details

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ import {
 
 import { Home } from "./src/screens/Home";
 import theme from "./src/styles/theme";
-import { Scheduling } from "./src/screens/Scheduling";
+import { SchedulingDetails } from "./src/screens/SchedulingDetails";
 
 export default function App() {
 	const [fontsLoaded] = useFonts({
@@ -34,7 +34,7 @@ export default function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			{/* <Home /> */}
-			<Scheduling />
+			<SchedulingDetails />
 		</ThemeProvider>
 	);
 }
--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -2,17 +2,54 @@ import React from "react";
 import { Feather } from "@expo/vector-icons";
 import { useTheme } from "styled-components";
 
-import { Calendar as CustomCalendar, LocaleConfig } from "react-native-calendars";
+import {
+	Calendar as CustomCalendar,
+	LocaleConfig,
+} from "react-native-calendars";
 
-LocaleConfig.locales['pt-br'] = {
-    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
-    dayNames: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
-    dayNamesShort: ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SÁB'],
-    today: 'Hoje'
+LocaleConfig.locales["pt-br"] = {
+	monthNames: [
+		"Janeiro",
+		"Fevereiro",
+		"Março",
+		"Abril",
+		"Maio",
+		"Junho",
+		"Julho",
+		"Agosto",
+		"Setembro",
+		"Outubro",
+		"Novembro",
+		"Dezembro",
+	],
+	monthNamesShort: [
+		"Jan",
+		"Fev",
+		"Mar",
+		"Abr",
+		"Mai",
+		"Jun",
+		"Jul",
+		"Ago",
+		"Set",
+		"Out",
+		"Nov",
+		"Dez",
+	],
+	dayNames: [
+		"Domingo",
+		"Segunda",
+		"Terça",
+		"Quarta",
+		"Quinta",
+		"Sexta",
+		"Sábado",
+	],
+	dayNamesShort: ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SÁB"],
+	today: "Hoje",
 };
 
-LocaleConfig.defaultLocale = 'pt-br'
+LocaleConfig.defaultLocale = "pt-br";
 
 export function Calendar() {
 	const theme = useTheme();
@@ -25,15 +62,13 @@ export function Calendar() {
 					name={direction == "left" ? "chevron-left" : "chevron-right"}
 				/>
 			)}
-
 			headerStyle={{
 				backgroundColor: theme.colors.background_secondary,
 				borderBottomWidth: 0.5,
-				borderBottomColor:theme.colors.text_detail,
+				borderBottomColor: theme.colors.text_detail,
 				paddingBottom: 10,
 				marginBottom: 10,
 			}}
-
 			theme={{
 				textDayFontFamily: theme.fonts.primary_400,
 				textDayHeaderFontFamily: theme.fonts.primary_500,
@@ -42,10 +77,9 @@ export function Calendar() {
 				textMonthFontSize: 20,
 				monthTextColor: theme.colors.title,
 				arrowStyle: {
-					marginHorizontal: -15
-				}
+					marginHorizontal: -15,
+				},
 			}}
-
 			firstDay={1}
 			minDate={new Date().toISOString()}
 		/>
